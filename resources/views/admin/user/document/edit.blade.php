bf069bf33c12a4b96bbbbd2cf593e4030f43fb55
AsfandRanglerz <asfand.ranglerz@gmail.com>
2023-02-10 16:57:22 +0500

    changes

--- resources/views/admin/user/document/edit.blade.php
+++ resources/views/admin/user/document/edit.blade.php
@@ -16,96 +16,97 @@
                             <div class="col-12 col-md-12 col-lg-12">
                                 <div class="card">
                                     <h4 class="text-center my-4">Edit Document</h4>
-                                    <div class="row mx-0 px-4">
-                                        <div class="col-sm-6 pl-sm-0 pr-sm-3">
-                                            <div class="form-group mb-2">
-                                                <label>Document Type<span class="required"> *</span></label>
-                                                <select id="selectDocument" class="form-control category" name="doc_type[]"
-                                                    value="{{ $data['doc_type'] }}" required>
+                                    <div id="docField1" class="doc-fields">
+                                        <div class="row mx-0 px-4">
+                                            <div class="col-sm-6 pl-sm-0 pr-sm-3">
+                                                <div class="form-group mb-2">
+                                                    <label>Document Type<span class="required"> *</span></label>
+                                                    <select id="selectDocument" class="form-control category" name="doc_type[]"
+                                                        value="{{ $data['doc_type'] }}" required>
 
-                                                    <option value="" selected disabled>Select Document</option>
-                                                    <option value="Passport"
-                                                        {{ $data['doc_type'] == 'Passport' ? 'selected' : '' }}>Passport
-                                                    </option>
-                                                    <option value="Identitiy Card"
-                                                        {{ $data['doc_type'] == 'Identitiy Card' ? 'selected' : '' }}>
-                                                        Identitiy Card</option>
-                                                    <option value="Visa"
-                                                        {{ $data['doc_type'] == 'Visa' ? 'selected' : '' }}>Visa</option>
-                                                    <option value="Insurance Card"
-                                                        {{ $data['doc_type'] == 'Insurance Card' ? 'selected' : '' }}>
-                                                        Insurance Card</option>
-                                                    <option value="Work Permit"
-                                                        {{ $data['doc_type'] == 'Work Permit' ? 'selected' : '' }}>Work
-                                                        Permit</option>
-                                                    <option value="Driving License"
-                                                        {{ $data['doc_type'] == 'Driving License' ? 'selected' : '' }}>
-                                                        Driving License</option>
-                                                    <option value="Other"
-                                                        {{ $data['doc_type'] == 'Other' ? 'selected' : '' }}>Other</option>
-                                                </select>
-                                                @error('doc_type')
-                                                    <div class="text-danger">{{ $message }}</div>
-                                                @enderror
+                                                        <option value="" selected disabled>Select Document</option>
+                                                        <option value="Passport"
+                                                            {{ $data['doc_type'] == 'Passport' ? 'selected' : '' }}>Passport
+                                                        </option>
+                                                        <option value="Identitiy Card"
+                                                            {{ $data['doc_type'] == 'Identitiy Card' ? 'selected' : '' }}>
+                                                            Identitiy Card</option>
+                                                        <option value="Visa"
+                                                            {{ $data['doc_type'] == 'Visa' ? 'selected' : '' }}>Visa</option>
+                                                        <option value="Insurance Card"
+                                                            {{ $data['doc_type'] == 'Insurance Card' ? 'selected' : '' }}>
+                                                            Insurance Card</option>
+                                                        <option value="Work Permit"
+                                                            {{ $data['doc_type'] == 'Work Permit' ? 'selected' : '' }}>Work
+                                                            Permit</option>
+                                                        <option value="Driving License"
+                                                            {{ $data['doc_type'] == 'Driving License' ? 'selected' : '' }}>
+                                                            Driving License</option>
+                                                        <option value="Other"
+                                                            {{ $data['doc_type'] == 'Other' ? 'selected' : '' }}>Other</option>
+                                                    </select>
+                                                    @error('doc_type')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6 pl-sm-0 pr-sm-3">
+                                                <div class="form-group mb-2">
+                                                    <label>Select File<span class="required"> *</span></label>
+                                                    <input type="file" name="file" value="{{ $data['file'] }}"
+                                                        class="form-control">
+                                                    @error('file')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
+
                                             </div>
                                         </div>
-                                        <div class="col-sm-6 pl-sm-0 pr-sm-3">
-                                            <div class="form-group mb-2">
-                                                <label>Select File<span class="required"> *</span></label>
-                                                <input type="file" name="file" value="{{ $data['file'] }}"
-                                                    class="form-control">
-                                                @error('file')
-                                                    <div class="text-danger">{{ $message }}</div>
-                                                @enderror
+                                        <div class="row mx-0 px-4">
+                                            <div class="col-sm-6 pl-sm-0 pr-sm-3 other-show d-none">
+                                                <div class="form-group mb-2">
+                                                    <label>Document Name<span class="required"> *</span></label>
+                                                    <input type="text" name="doc_name[]" value="{{ old('doc_name[]') }}"
+                                                        placeholder="Enter Document Name" class="form-control" required>
+                                                    @error('doc_name')
+                                                        <div class="text-danger p-2">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
                                             </div>
+                                            <div class="col-sm-6 pl-sm-0 pr-sm-3 other-none">
+                                                <div class="form-group mb-2">
+                                                    <label>Issue Date<span class="required"> *</span></label>
+                                                    <input type="date" name="issue_date" value="{{ $data['issue_date'] }}"
+                                                        id="issue_date" class="form-control" placeholder="Issue Date">
+                                                    @error('issue_date')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6 pl-sm-0 pr-sm-3 other-none">
+                                                <div class="form-group mb-2">
+                                                    <label>Expiry Date<span class="required"> *</span></label>
+                                                    <input type="date" name="expiry_date" value="{{ $data['expiry_date'] }}"
+                                                        id="expiry_date" class="form-control" placeholder="Expiry Date">
+                                                    @error('expiry_date')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
 
+                                            </div>
+                                        </div>
+                                        <div class="row mx-0 px-4">
+                                            <div class="col-sm-12 pl-sm-0 pr-sm-3">
+                                                <div class="form-group mb-2">
+                                                    <label>Comments</label>
+                                                    <textarea placeholder="Enter Your Comments ..." name="comment" id="comment"  class="form-control">{{ $data['comment'] ?? '' }}</textarea>
+                                                    @error('comment')
+                                                        <div class="text-danger">{{ $message }}</div>
+                                                    @enderror
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="row mx-0 px-4">
-                                        <div class="col-sm-6 pl-sm-0 pr-sm-3 other-show d-none">
-                                            <div class="form-group mb-2">
-                                                <label>Document Name<span class="required"> *</span></label>
-                                                <input type="text" name="doc_name[]" value="{{ old('doc_name[]') }}"
-                                                    placeholder="Enter Document Name" class="form-control" required>
-                                                @error('doc_name')
-                                                    <div class="text-danger p-2">{{ $message }}</div>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6 pl-sm-0 pr-sm-3">
-                                            <div class="form-group mb-2">
-                                                <label>Issue Date<span class="required"> *</span></label>
-                                                <input type="date" name="issue_date" value="{{ $data['issue_date'] }}"
-                                                    id="issue_date" class="form-control" placeholder="Issue Date">
-                                                @error('issue_date')
-                                                    <div class="text-danger">{{ $message }}</div>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6 pl-sm-0 pr-sm-3">
-                                            <div class="form-group mb-2">
-                                                <label>Expiry Date<span class="required"> *</span></label>
-                                                <input type="date" name="expiry_date" value="{{ $data['expiry_date'] }}"
-                                                    id="expiry_date" class="form-control" placeholder="Expiry Date">
-                                                @error('expiry_date')
-                                                    <div class="text-danger">{{ $message }}</div>
-                                                @enderror
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                    <div class="row mx-0 px-4">
-                                        <div class="col-sm-12 pl-sm-0 pr-sm-3">
-                                            <div class="form-group mb-2">
-                                                <label>Comments</label>
-                                                <textarea placeholder="Enter Your Comments ..." name="comment" id="comment"  class="form-control">{{ $data['comment'] ?? '' }}</textarea>
-                                                @error('comment')
-                                                    <div class="text-danger">{{ $message }}</div>
-                                                @enderror
-                                            </div>
-                                        </div>
-                                    </div>
-
                                     <div class="card-footer text-center row">
                                         <div class="col">
                                             <button type="submit" class="btn btn-success mr-1 btn-bg"
