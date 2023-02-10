107a95bddf5a238e35ecfdb6fd73bdeb87cd642c
AsfandRanglerz <asfand.ranglerz@gmail.com>
2023-02-10 16:05:12 +0500

    changes

--- resources/views/admin/common/footer.blade.php
+++ resources/views/admin/common/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="footer-left">
-        <p>Powered By <a href="https://www.ranglerz.com/" target="_blank">RANGLERZ</a></p>
+        <p>Powered By <a href="https://www.ranglerz.com/" target="_blank" style="color: #FFF!important">RANGLERZ</a></p>
     </div>
     <div class="footer-right">
     </div>
